9db6056653cf2ff0afb05fae2bfebca4a9824948
Jad Aldaim <aldaimjad1@gmail.com>
2022-10-07 19:20:19 +0300

    main/fixed itemFull component bag

--- src/components/homepage/homepage.tsx
+++ src/components/homepage/homepage.tsx
@@ -10,39 +10,6 @@ export default function Homepage(){
     const dispatch = useDispatch();
 
     const showApp = useAppSelector(({dragonSlice}) => !dragonSlice.isLoading && !dragonSlice.error);
-    // const savedInStorage = useAppSelector(({dragonSlice}) => dragonSlice.savedInStorage);
-    //
-    // useEffect(() => {
-    //     !savedInStorage && dispatch(dragonSlice.actions.startLoading());
-    //     savedInStorage && dispatch(dragonSlice.actions.setDataFromLocalStorage());
-    //     API.get('')
-    //         .then(({data}) => {
-    //             dispatch(dragonSlice.actions.getSuccess(data.map((dragon:any) => {
-    //                 return {
-    //                     id: dragon.id,
-    //                     name: dragon.name,
-    //                     images: dragon.flickr_images,
-    //                     description: dragon.description,
-    //                     wikipedia: dragon.wikipedia,
-    //                     first_flight: {
-    //                         title: 'First flight',
-    //                         content: dragon.first_flight.replaceAll('-', '.')
-    //                     },
-    //                     diameter: {
-    //                         title: 'Diameter',
-    //                         content: dragon.diameter.meters + ' m',
-    //                     },
-    //                     dry_mass: {
-    //                         title: 'Dry mass',
-    //                         content: dragon.dry_mass_kg + ' kg',
-    //                     }
-    //                 }
-    //             })));
-    //             dispatch(dragonSlice.actions.stopLoading());
-    //         })
-    //         .catch(({message}) => !savedInStorage && dispatch(dragonSlice.actions.failed(message)))
-    // },[])
-
     return (
         <>
             {showApp && (
--- src/components/item-full/item-full.tsx
+++ src/components/item-full/item-full.tsx
@@ -3,29 +3,37 @@ import DragonHeader from "../dragon-header/dragon-header";
 import Slider from "../slider/slider";
 import Description from "../description/description";
 import Params from "../params/params";
-import {NavLink, useNavigate, useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {useAppSelector} from "../../hooks/redux";
 import DragonI from "../../models/dragon";
 import {AiFillHome} from "react-icons/ai";
+import { useState } from 'react';
 
 export default function ItemFull():JSX.Element{
+    const [hide,setHide] = useState<boolean>(false)
+
     const navigate = useNavigate();
     const params = useParams();
+
     const id = params.id;
     const dragon = useAppSelector(({dragonSlice}) => dragonSlice.dragons.find(dragon => dragon.id === id)) as DragonI;
     return (
         <>
-            <div className={styles.container}>
+            {dragon && (<div className={`${styles.container} ${hide ? styles.hide : ''}`}>
                 <DragonHeader dragon={dragon}/>
                 <Slider dragon={dragon}/>
                 <Description dragon={dragon}/>
                 <Params dragon={dragon}/>
                 <a
-                    onClick={() => navigate('/home')}
+                    onClick={() => {
+                        setHide(true);
+                        setTimeout(() => navigate('/home'),1000)
+                        // navigate('/home');
+                    }}
                     children={<AiFillHome style={{fill: "white",maxWidth:'20px',maxHeight:'20px'}}/>}
                     className={styles.home}
                 />
-            </div>
+            </div>)}
         </>
     )
 }
