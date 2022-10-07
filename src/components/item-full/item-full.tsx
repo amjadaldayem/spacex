598d68f79a47e2b5bfb1897863f95196701a18c9
Jad Aldaim <aldaimjad1@gmail.com>
2022-10-07 17:15:48 +0300

    main/home button

--- src/components/item-full/item-full.tsx
+++ src/components/item-full/item-full.tsx
@@ -3,12 +3,13 @@ import DragonHeader from "../dragon-header/dragon-header";
 import Slider from "../slider/slider";
 import Description from "../description/description";
 import Params from "../params/params";
-import {NavLink, useParams} from "react-router-dom";
+import {NavLink, useNavigate, useParams} from "react-router-dom";
 import {useAppSelector} from "../../hooks/redux";
 import DragonI from "../../models/dragon";
 import {AiFillHome} from "react-icons/ai";
 
 export default function ItemFull():JSX.Element{
+    const navigate = useNavigate();
     const params = useParams();
     const id = params.id;
     const dragon = useAppSelector(({dragonSlice}) => dragonSlice.dragons.find(dragon => dragon.id === id)) as DragonI;
@@ -19,8 +20,8 @@ export default function ItemFull():JSX.Element{
                 <Slider dragon={dragon}/>
                 <Description dragon={dragon}/>
                 <Params dragon={dragon}/>
-                <NavLink
-                    to={'/home'}
+                <a
+                    onClick={() => navigate('/home')}
                     children={<AiFillHome style={{fill: "white",maxWidth:'20px',maxHeight:'20px'}}/>}
                     className={styles.home}
                 />
